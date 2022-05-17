6069da7d2bf08ef5656ca390b379735817185a12
zaak <zaak.kerstetter@gmail.com>
2022-05-17 15:13:32 -0600

    commit tree 6a51167847b9f66b7e05a75b0d3686456bd1b8aa

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let names = ["Charlie", "Samip", "Ali"]
+let events = "surprise"
+
+function writeCards(names, events){
+   let thankYouCards = []
+    for (let i = 0; i < names.length; i++){
+       thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+    }
+    return thankYouCards
+}
+
+function countDown(x){
+    while (x >= 0){
+        console.log(x--)}
+}
